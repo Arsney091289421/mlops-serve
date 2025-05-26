467c0e98d2e62ffffc3d0bb31f98e86a81b2bcf0
Arsney091289421 <151865017+Arsney091289421@users.noreply.github.com>
2025-05-26 00:00:12 -0400

    Refactor: moved top-level execution code into @app startup event and added model download on service start

--- app/main.py
+++ app/main.py
@@ -13,17 +13,28 @@ from app.utils import (
     predict_issues
 )
 
-# ===== Load config =====
+# Load environment variables 
 load_dotenv()
 MODEL_DIR = os.getenv("MODEL_DIR", "./model")
 MODEL_PATH = os.path.join(MODEL_DIR, "latest_model.json")
 CSV_OUTPUT_DIR = os.getenv("CSV_OUTPUT_DIR", "./predict_outputs")
-MODEL_BUCKET = os.getenv("MODEL_BUCKET")   # .env
+MODEL_BUCKET = os.getenv("MODEL_BUCKET")
 MODEL_S3_KEY = os.getenv("MODEL_S3_KEY", "model/latest_model.json")
+
 os.makedirs(CSV_OUTPUT_DIR, exist_ok=True)
-download_model_from_s3(MODEL_BUCKET, MODEL_S3_KEY, MODEL_DIR) # Make sure using the laetst model
 
 app = FastAPI()
+
+# Download the model on app startup, runs only during runtime
+@app.on_event("startup")
+def startup_download_model():
+    if MODEL_BUCKET and MODEL_S3_KEY:
+        print(f"[INFO] Downloading model from S3 bucket: {MODEL_BUCKET}, key: {MODEL_S3_KEY}")
+        download_model_from_s3(MODEL_BUCKET, MODEL_S3_KEY, MODEL_DIR)
+    else:
+        print("[INFO] Skip S3 download: Missing MODEL_BUCKET or MODEL_S3_KEY.")
+
+# API Routes 
 
 @app.get("/predict_recent_open_issues")
 def predict_recent_open_issues(days: int = Query(1, description="How many recent days")):
@@ -59,5 +70,5 @@ def export_predictions_by_ids(ids: List[int] = Body(..., embed=True)):
     df.to_csv(out_path, index=False)
     return FileResponse(out_path, media_type="text/csv", filename=os.path.basename(out_path))
 
-# ==== Prometheus metrics ====
+# Prometheus metrics 
 Instrumentator().instrument(app).expose(app)
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,4 +1,3 @@
 def test_imports():
     from app import utils
-    from app import main
     assert True # Make sure there will be no warnings
